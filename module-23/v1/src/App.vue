38a1da6d91863d028eb7680077099ad983ff88b7
biplabuddin99 <biplabuddin990@gmail.com>
2024-12-19 15:06:09 +0600

    dynamic data print vue

--- module-23/v1/src/App.vue
+++ module-23/v1/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-
+import AwesomeAlert from './components/AwesomeAlert.vue';
 </script>
 
 <template>
-<h1>Welcome To VUE</h1>
+<h1 class="text-3xl">Welcome To VUE</h1>
+<AwesomeAlert heading="Wow" message="This is component"/>
+<awesome-alert/>
 </template>
 
 <style scoped>
---
+++ module-23/v1/src/components/AwesomeAlert.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps([
+    'heading',
+    'message'
+])
+</script>
+<template>
+<div class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+    <span class="font-medium">Info alert!</span> change a few things up and try submitting again.
+    <br/>
+    <span class="font-medium">{{ heading }}</span>{{message}}
+</div>
+</template>
+<style scoped>
+</style>
