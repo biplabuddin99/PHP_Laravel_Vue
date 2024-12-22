489e1170138eaa1d7d45275b812865b7423aa03b
biplabuddin99 <biplabuddin990@gmail.com>
2024-12-22 16:07:42 +0600

    git template conversion with component

---
+++ module-23/v2/src/components/ContactSection.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ContactInfo from './contact/ContactInfo.vue';
+import ContactForm from './contact/ContactForm.vue';
+</script>
+<template>
+<section class="p-4 py-20">
+    <div class="mx-auto max-w-7xl w-full">
+      <div class="grid lg:grid-cols-2 grid-cols-1 items-center">
+        <contact-info  phone="01628682796" email="biplab@gmail.com" address="Chottagram,Bangladesh"/>
+        <contact-form :displayPhone="false"/>
+      </div>
+    </div>
+  </section>
+</template>
+<style scoped></style>
---
+++ module-23/v2/src/components/contact/ContactForm.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue';
+defineProps({
+    displayPhone:{
+        type:Boolean,
+        default:true
+    }
+})
+const name=ref('')
+const email=ref('')
+const phone=ref('')
+const message=ref('')
+function sendMessage(){
+    console.log(name.value,email.value,phone.value,message.value);
+}
+</script>
+<template>
+            <div class="p-5 lg:p-11 lg:rounded-r-2xl rounded-2xl">
+          <h2 class="font-manrope text-4xl font-semibold leading-10 mb-11">Send Us A Message</h2>
+          <input type="text" v-model="name"
+            class="w-full h-12 text-gray-600 placeholder-gray-400  shadow-sm bg-transparent text-lg font-normal leading-7 border border-black focus:outline-none p-4 mb-10"
+            placeholder="Name">
+          <input type="text" v-model="email"
+            class="w-full h-12 text-gray-600 placeholder-gray-400 shadow-sm bg-transparent text-lg font-normal leading-7 border border-black focus:outline-none p-4 mb-10"
+            placeholder="Email">
+          <input type="text" v-show="displayPhone" v-model="phone"
+            class="w-full h-12 text-gray-600 placeholder-gray-400 shadow-sm bg-transparent text-lg font-normal leading-7 border border-black focus:outline-none p-4 mb-10"
+            placeholder="Phone">
+          <textarea v-model="message"
+            class="max-h-[200px] w-full text-gray-600 placeholder-gray-400 bg-transparent text-lg shadow-sm font-normal leading-7 border border-black focus:outline-none p-4 mb-10"
+            placeholder="Message"></textarea>
+          <button @click="sendMessage()"
+            class="w-full h-12 text-white text-base font-semibold leading-6 rounded-full transition-all duration-700 bg-black shadow-sm">Send</button>
+        </div>
+</template>
+<style scoped></style>
