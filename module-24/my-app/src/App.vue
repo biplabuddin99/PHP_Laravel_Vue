008dfb6ca23f63bc803dfc7034a8ddb3daedf8f1
biplabuddin99 <biplabuddin990@gmail.com>
2024-12-31 17:58:21 +0600

    update

--- module-24/my-app/src/App.vue
+++ module-24/my-app/src/App.vue
@@ -1,25 +1,49 @@
 <template>
- <Part1/>
- <hr>
- <Part2/>
- <hr>
- <Part3/>
- {{ products }}
+  <Part1 />
+  <hr>
+  <Part2 />
+  <hr>
+  <Part3 />
+  <!-- {{ products }} -->
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="table">
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Category</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in products">
+                <td>{{ item.title }}</td>
+                <td>{{ item.category }}</td>
+                <td>{{ item.price }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-</style>
+
+<style scoped></style>
 <script setup>
 import Part1 from './components/Part1.vue';
 import Part2 from './components/Part2.vue';
 import Part3 from './components/Part3.vue';
 import { ref } from 'vue';
-const products=ref([]);
+const products = ref([]);
 CallApi();
-async function CallApi(){
+async function CallApi() {
   const response = await fetch("https://dummyjson.com/products");
-  const data =await response.json();
+  const data = await response.json();
   console.log(data);
-  products.value=data;
+  products.value = data['products'];
 }
 </script>
