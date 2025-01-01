75d4ad393540fd14181c424e4e4598885f2e1821
biplabuddin99 <biplabuddin990@gmail.com>
2025-01-01 17:09:44 +0600

    module-24 part-2 static color

---
+++ module-24/state/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+const color ='red';
+</script>
+
+<template>
+<h1>Hello</h1>
+<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded">Button</button>
+
+</template>
+
+<style scoped>
+
+</style>
