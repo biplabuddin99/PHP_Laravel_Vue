1c72be98018826dd6fb1571cd930f5284b30f5b1
biplabuddin99 <biplabuddin990@gmail.com>
2025-01-01 17:59:03 +0600

    update

--- module-24/state/src/App.vue
+++ module-24/state/src/App.vue
@@ -1,10 +1,19 @@
 <script setup>
-const color ='red';
+import { ref } from 'vue';
+const color =ref('red');
+const fontSize=ref("28");
 </script>
 
 <template>
-<h1>Hello</h1>
-<button class="text-white font-bold py-2 px-2 rounded" :class="`bg-${color}-500 hover:bg-${color}-700`">Button</button>
+<!-- <h1>Hello</h1> -->
+<button class="text-white font-bold py-2 px-2 rounded" 
+:class="`bg-${color}-500 hover:bg-${color}-700`"
+:style="`font-size:${fontSize}px`"
+>Button</button>
+<div class="">
+  <input type="text" class="border border-gray-400 p-2" v-model="color" placeholder="color">
+<input type="number" class="border border-gray-400 p-2" v-model="fontSize" placeholder="fontSize">
+</div>
 
 </template>
 
