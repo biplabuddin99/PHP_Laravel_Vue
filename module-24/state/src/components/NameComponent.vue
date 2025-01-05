5d27677d918807c1e02ff01c6a5b5be91a6377d9
biplabuddin99 <biplabuddin990@gmail.com>
2025-01-05 12:03:44 +0600

    Name Component

---
+++ module-24/state/src/components/NameComponent.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps([ 'firstName','lastName' ])
+</script>
+<template>
+ <h2>From Component {{ firstName }} {{ lastName }}</h2>
+ <input type="text" placeholder="first Name">
+</template>
+
+<style scoped>
+</style>
---
+++ module-24/state/src/components/StyleClassBind.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue';
+import {reactive} from 'vue';
+// const color =ref('red');
+// const fontSize=ref("28");
+const theme=reactive({
+  buttonColor:'lime',
+  buttonTextSize:'43'
+})
+</script>
+
+<template>
+<!-- <h1>Hello</h1> -->
+<!-- <button class="text-white font-bold py-2 px-2 rounded" 
+:class="`bg-${color}-500 hover:bg-${color}-700`"
+:style="`font-size:${fontSize}px`"
+>Button</button>
+<div class="">
+  <input type="text" class="border border-gray-400 p-2" v-model="color" placeholder="color">
+<input type="number" class="border border-gray-400 p-2" v-model="fontSize" placeholder="fontSize">
+</div> -->
+<button class="text-white font-bold py-2 px-2 rounded" 
+:class="`bg-${theme.buttonColor}-500 hover:bg-${theme.buttonColor}-700`"
+:style="`font-size:${theme.buttonTextSize}px`"
+>Button</button>
+<div class="">
+  <input type="text" class="border border-gray-400 p-2" v-model="theme.buttonColor" placeholder="color">
+<input type="number" class="border border-gray-400 p-2" v-model="theme.buttonTextSize" placeholder="fontSize">
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
