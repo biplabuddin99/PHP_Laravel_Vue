b4268ddcb6eb54ffa3e7ce484a604a1766238de7
biplabuddin99 <biplabuddin990@gmail.com>
2025-01-08 16:16:29 +0600

    module-26 input data bind

---
+++ module-26/my-app/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {ref} from "vue";
+const firstName=ref('')
+</script>
+
+<template>
+<h1>Vue Js Form </h1>
+<input type="text" v-model="firstName" placeholder="first name"/>{{ firstName }}
+</template>
+
+<style scoped>
+
+</style>
