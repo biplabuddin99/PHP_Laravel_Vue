6f239ca8ee1924c6fd8d2dfe67e29020937a23e3
biplabuddin99 <biplabuddin990@gmail.com>
2025-07-13 17:40:16 +0600

    add ecommarch-project backend.

---
+++ module-28/part-2/my-app/routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\TokenVerificationMiddleware;
+use Illuminate\Support\Facades\Route;
+
+
+// user
+Route::post('/user-registration', [UserController::class, 'userRegistration'])->name('userRegistration');
+Route::post('/user-login', [UserController::class, 'userLogin'])->name('userLogin');
+Route::get('/logout', [UserController::class, 'userLogout'])->name('userLogout');
+Route::post('/send-otp', [UserController::class, 'SendOTPCode'])->name('SendOTPCode');
+Route::post('/verify-otp', [UserController::class, 'VerifyOTP'])->name('VerifyOTP');
+Route::post('/reset-password',[UserController::class,'ResetPassword'])->middleware([TokenVerificationMiddleware::class]);
+Route::get('/user-profile',[UserController::class,'UserProfile'])->middleware([TokenVerificationMiddleware::class]);
+Route::post('/user-update',[UserController::class,'UpdateProfile'])->middleware([TokenVerificationMiddleware::class]);
+
+
+// Category
+Route::post("/create-category",[CategoryController::class,'CategoryCreate'])->middleware([TokenVerificationMiddleware::class]);
+Route::get("/list-category",[CategoryController::class,'CategoryList'])->middleware([TokenVerificationMiddleware::class]);
+Route::post("/delete-category",[CategoryController::class,'CategoryDelete'])->middleware([TokenVerificationMiddleware::class]);
+Route::post("/update-category",[CategoryController::class,'CategoryUpdate'])->middleware([TokenVerificationMiddleware::class]);
+Route::post("/category-by-id",[CategoryController::class,'CategoryByID'])->middleware([TokenVerificationMiddleware::class]);
+
+
+// Customer API
+Route::post("/create-customer",[CustomerController::class,'CustomerCreate'])->middleware([TokenVerificationMiddleware::class]);
+Route::get("/list-customer",[CustomerController::class,'CustomerList'])->middleware([TokenVerificationMiddleware::class]);
+Route::post("/delete-customer",[CustomerController::class,'CustomerDelete'])->middleware([TokenVerificationMiddleware::class]);
+Route::post("/update-customer",[CustomerController::class,'CustomerUpdate'])->middleware([TokenVerificationMiddleware::class]);
+Route::post("/customer-by-id",[CustomerController::class,'CustomerByID'])->middleware([TokenVerificationMiddleware::class]);
